eb7fd24bc4205a35fa6f3c1d76cb9e0728790df1
Mustafa SAYDAN <mailadresin@example.com>
2025-07-22 14:41:15 +0300

    .

--- auth/routes.py
+++ auth/routes.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, request, jsonify, current_app
 from auth.services import kullanici_kaydet, kullanici_dogrula
-from auth.validators import eksik_alan_kontrol  # Bunu daha sonra oluşturacağız
+from auth.validators import eksik_alan_kontrol 
 import jwt
 import datetime
 
